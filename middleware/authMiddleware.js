72000bf5451c65ffb6c5160531c19ad9c35406d8
Gerrard2022 <110633144+Gerrard2022@users.noreply.github.com>
2022-09-16 20:12:13 +0200

    Add files via upload

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,50 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const requireAuth = (req, res, next) => {
+
+    const token = req.cookies.jwt;
+
+    // check json web token exits & is verified
+    if(token){
+        jwt.verify(token, 'Itachi Uchiha', (err, decodedToken) => {
+            if(err){
+                res.redirect('/login');
+            }
+            else{
+                next();
+            }
+        })
+    }
+    else{
+        res.redirect('/login');
+    }
+
+}
+
+// check current user
+const checkUser = (req, res, next) => {
+ 
+    const token = req.cookies.jwt;
+
+    if(token){
+        jwt.verify(token, 'Itachi Uchiha', async (err, decodedToken) => {
+            if(err){
+                res.locals.user = null;
+                next();
+            }
+            else{
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user; 
+                next();
+            }
+        })
+    }
+    else{
+        res.locals.user = null;
+        next();
+    }
+
+}
+
+module.exports = { requireAuth, checkUser };
